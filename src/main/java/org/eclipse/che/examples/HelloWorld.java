e8a9433f4dfeaca97b92f2914d270117638cd504
Huma <work.samp01@gmail.com>
2017-01-10 09:52:02 +0000

    ch

--- src/main/java/org/eclipse/che/examples/HelloWorld.java
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -3,6 +3,7 @@ package org.eclipse.che.examples;
 public class HelloWorld {
     public static void main(String[] argvs) {
         String a = "Che";
+        System.out.println("Test");
         System.out.println("Hello World " + a + "!");
     }
 }
