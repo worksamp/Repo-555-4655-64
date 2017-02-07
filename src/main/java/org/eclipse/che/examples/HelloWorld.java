f92a5fef1159cd0a59ebffeb2b346e3ccaed28f3
Nikita <work.samp01@gmail.com>
2017-02-07 12:24:34 +0000

    added for testing

--- src/main/java/org/eclipse/che/examples/HelloWorld.java
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -3,6 +3,7 @@ package org.eclipse.che.examples;
 public class HelloWorld {
     public static void main(String[] argvs) {
         String a = "Che";
+        // added during testing
         System.out.println("Hello World " + a + "!");
     }
 }
