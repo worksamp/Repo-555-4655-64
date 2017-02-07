091f8fa057a675d1cf045e34891ab541b4f3ae61
Nikita <work.samp01@gmail.com>
2017-02-07 13:59:11 +0000

    demo 8th feb

--- src/main/java/org/eclipse/che/examples/HelloWorld.java
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] argvs) {
         String a = "Che";
         // added during testing
+        // added during testing 8th feb
         System.out.println("Hello World " + a + "!");
     }
 }
